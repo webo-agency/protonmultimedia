beaec86d347c096331eba19348b9615c9797887e
Tomek A <tomasz_adamczyk@hotmail.com>
2020-08-04 19:34:58 +0200

    RADEK-26 build hero unit swiper

--- theme/front-page.php
+++ theme/front-page.php
@@ -2,16 +2,68 @@
 /**
  * The homepage template file
  *
- * @package Swistak_Theme
+ * @package ProtonMultimedia
  */
 
 get_header();
 
 ?>
-	<section class="pm-baner">
-		<div class="pm-container">
-		
+	<section class="pm-baner">	
+		<div class="pm-swiper pm-swiper--hero-swiper">
+			<div class="swiper-container pm-swiper--hero-swiper__container" data-slider-hero>
+				<ul class="swiper-wrapper">
+				<?php
+					if( have_rows('front_slider_slide') ):
+						while ( have_rows('front_slider_slide') ) : the_row();
+							$heading = get_sub_field('front_slide_heading');
+							$content = get_sub_field('front_slide_content');
+							$front_slide_button = get_sub_field('front_slide_button');
+							$front_slider_image = get_sub_field('front_slider_image');
+							?>	
+								<li class="swiper-slide" style="background: url(<?php echo $front_slider_image['url'] ?>) center center no-repeat; background-size: cover">
+									<!-- <img
+										class="swiper-lazy"
+										data-src="<?php echo $front_slider_image['url']; ?>"
+										alt="<?php echo $front_slider_image['alt']; ?>"
+										title="<?php echo $front_slider_image['title']; ?>"
+									/> -->
+									<div class="pm-container pm-swiper--hero-swiper__content-container">
+										<?php echo $heading; ?>
+										<div class="pm-swiper--hero-swiper__content-wrapper">
+											<div class="pm-swiper--hero-swiper__content" data-hero-content>
+												<?php echo $content; ?>
+											</div>
+										</div>
+											<?php
+												if( $front_slide_button ): 
+													$front_slide_button_url = $front_slide_button['url'];
+													$front_slide_button_title = $front_slide_button['title'];
+													$front_slide_button_target = $front_slide_button['target'] ? $front_slide_button['target'] : '_self';
+												endif;
+											?>
+										<a
+											class="pm-button pm-button--primary pm-button--large"
+											href="<?php echo esc_url( $front_slide_button_url ); ?>"
+											target="<?php echo esc_attr( $front_slide_button_target ); ?>">
+											<?php echo esc_html( $front_slide_button_title ); ?>
+										</a>
+									</div>
+								</li>
+							<?php
+						endwhile;
+					else :
+					endif;
+				?>
+				</ul>
+				<div class="pm-container">
+			<div class="swiper-pagination pm-swiper--hero-swiper__swiper-pagination"></div>
+			</div>
+				</div>
 		</div>
+	</section>
+
+	<section style="padding: 100px 0; background: #110D25;">
+	
 	</section>
 <?php
 get_footer();
