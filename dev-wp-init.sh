d00f982bb5a7ca3cc4aa53f43294f29ead3350cf
Triloworld <patryk@padus.pl>
2020-08-09 12:02:54 +0200

    Docker

--- dev-wp-init.sh
+++ dev-wp-init.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 echo "------------ Installation in DEV mode ------------ "
 
 export WP_ENV=development
 echo 'ServerName localhost' >> /etc/apache2/apache2.conf
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 /usr/local/bin/php wp-cli.phar --info
-chmod +x wp-cli.phar
+chmod +x wp-cli.pha
 /usr/local/bin/php wp-cli.phar plugin activate --all --allow-root
 
 echo "------------ END INSTALLATION ------------"
