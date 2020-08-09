946f38e2fbaffe323ea93f19983c1e092cfef1c7
Triloworld <patryk@padus.pl>
2020-08-09 20:44:12 +0200

    Deploy test

--- theme/page.php
+++ theme/page.php
@@ -14,7 +14,7 @@
 
 get_header();
 
-get_template_part( 'template-parts/section', 'baner' );
+	get_template_part( 'template-parts/section', 'baner' );
 ?>
 
 	<div id="primary" class="content-area">
