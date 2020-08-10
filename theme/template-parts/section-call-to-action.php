7c92ada1206f329b8f1d3786ce446a0fc2ded864
Tomek A <tomasz_adamczyk@hotmail.com>
2020-08-10 19:58:14 +0200

    tailwind and cta settings

--- theme/template-parts/section-call-to-action.php
+++ theme/template-parts/section-call-to-action.php
@@ -1,1 +1,22 @@
-<?php echo file_get_contents( get_stylesheet_directory_uri() . '/assets/svg/call-to-action.svg' ); ?>
+
+<section class="pm-cta relative">
+    <img 
+        class="hidden lg:block absolute top-0 left-0 h-full z-50" 
+        src="<?php echo get_template_directory_uri() . '/assets/svg/lines-right.svg' ?>" 
+        alt="Decorations"
+    />
+    <div class="pm-container pm-container--indented">
+        <?php
+            $cta_title = get_field('cta_title', 'option');
+            $cta_button = get_field('cta_button', 'option');
+        ?>
+        <h3><?php echo $cta_title; ?></h3>
+        <a
+            class="pm-button pm-button--primary pm-button--large"
+            href="<?php echo $cta_button['url'] ?>"
+            target="<?php echo $cta_button['target'] ?>"
+        >
+            <?php echo $cta_button['title'] ?>
+        </a>
+    </div>
+</section>
