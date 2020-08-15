fe731c41759914f78b7692464311f18b091117bb
Tomek A <tomasz_adamczyk@hotmail.com>
2020-08-15 17:04:37 +0200

    RADEK-22 render post taxonomies

--- theme/template-parts/section-news.php
+++ theme/template-parts/section-news.php
@@ -2,15 +2,17 @@
 
 <div class="pm-container">
     <?php echo get_field('news_title');
-    $featured_posts = get_field('news_items');
-    if( $featured_posts ): ?>
-        <ul class="flex flex-col tablet:flex-row">
-        <?php foreach( $featured_posts as $featured_post ): 
+    $post_list = get_posts( array(
+        'sort_order' => 'desc',
+        'numberposts' => 4,
+    ) );
+    if( $post_list ): ?>
+        <ul class="flex flex-col tablet:flex-row tablet:flex-wrap">
+        <?php foreach( $post_list as $featured_post ): 
             $post_date = get_the_date( 'd / m / Y' ); 
             $permalink = get_permalink( $featured_post->ID );
             $title = get_the_title( $featured_post->ID );
             $excerpt = get_the_excerpt( $featured_post->ID);
-            $tags = get_field( 'news_tags', $featured_post->ID );
             $post_image = get_the_post_thumbnail_url($featured_post->ID,'medium_large');
         ?>
             
@@ -21,16 +23,41 @@
                         <span class="text-primary text-sm tablet:text-base leading-line-height-normal block mb-4"><?php echo $post_date; ?></span>
                         <h3 class="text-base tablet:text-md desktop:text-lg leading-line-height-normal tablet:leading-line-height-md desktop:leading-line-height-lg mb-4 full-hd:mb-10 font-weight-bold"><?php echo esc_html( $title ); ?></h3>
                         <div class="pm-news-article__teaser">
-                            <div class="hidden tablet:block mb-6 full-hd:mb-20">
-                                <?php if($tags) {foreach( $tags as $tag ): 
-                                    $term = get_term( $tag, $taxonomy );
-                                    $tag_link = get_field('tag_link', $term->taxonomy.'_'.$term->term_id);
-                                    ?>
-                                        <a class="rounded border-primary border-2 border-solid text-white desktop:text-black text-sm leading-line-height-sm p-2 inline-block" href="<?php  echo $tag_link['url']; ?>" title="<?php echo $tag_link['title']; ?>">
-                                            <?php echo $tag_link['title']; ?>
-                                        </a>
-                                <?php endforeach;
-                                } ?>
+                            <div class="hidden desktop:block mb-6 full-hd:mb-20">
+                                <?php 
+                                
+                                $post_services = get_field( 'services', $featured_post->ID );
+                                if($post_services) {
+                                    foreach($post_services as $post_service) {
+                    
+                                        $service = get_post($post_service);
+                                        $service_terms = wp_get_post_terms($post_service, 'services_tags', array("fields" => "all"));
+                                        $service_categories = wp_get_post_terms($post_service, 'services_category', array("fields" => "all"));
+                                        $term_id = false;
+                                        $service_term = '';
+                                        $service_category = '';
+                                        $post_taxonomies = array();
+
+                                        if ( $service_terms ) {
+                                            $term_id = $service_terms[0]->term_id;
+                                            $service_term = get_term($term_id);
+                                        }
+                                        
+                                        if ( $service_categories ) {
+                                            $term_id = $service_categories[0]->term_id;
+                                            $service_category = get_term($term_id);
+                                        }
+                                        array_push($post_taxonomies, $service_category, $service_term);
+
+                                        foreach($post_taxonomies as $post_taxonomy) {
+                                            ?>
+                                            <a class="rounded border-primary border-2 border-solid text-white desktop:text-black text-sm leading-line-height-sm p-2 inline-block" href="<?php echo get_term_link( $post_taxonomy ); ?>" title="<?php echo $post_taxonomy->name; ?>">
+                                                <?php echo $post_taxonomy->name; ?>
+                                            </a>
+                                            <?php
+                                        }
+                                    }
+                                }?>
                             </div>
                             <div class="hidden text-sm full-hd:text-base desktop:block mb-6 full-hd:mb-20 text-black">
                                 <?php echo $excerpt; ?>
@@ -45,4 +72,3 @@
         </ul>
     <?php endif; ?>
 </div>
-<!--opacity-0 -translate-y-40 invisible-->
