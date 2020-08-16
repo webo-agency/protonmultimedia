838311fc8ca1d03930fa35da95951b1607f5c302
Triloworld <patryk@padus.pl>
2020-08-16 13:39:23 +0200

    Offer

--- theme/template-parts/section-offer.php
+++ theme/template-parts/section-offer.php
@@ -1,7 +1,3 @@
-<section height="638px" class="overflow-hidden">
-  <?php echo file_get_contents( get_stylesheet_directory_uri() . '/assets/svg/offer.svg' ); ?>
-</section>
-
 <section>
   <?php
     $terms = get_terms( array(
