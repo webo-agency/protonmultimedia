03fed6577aecc1c1b78889ff3b9f657d3d44157f
Tomek A <tomasz_adamczyk@hotmail.com>
2020-08-16 17:06:27 +0200

    RADEK-24 about section template

--- theme/template-parts/section-about-us.php
+++ theme/template-parts/section-about-us.php
@@ -1,3 +1,34 @@
-<section style="547.8px" class="overflow-hidden">
-  <?php echo file_get_contents( get_stylesheet_directory_uri() . '/assets/svg/about-us.svg' ); ?>
+<section class="bg-dark-blue-2 relative">
+    <img    
+        class="hidden tablet:block absolute top-0 desktop:bottom-100px desktop:top-auto desktop-wide:bottom-0 right-0 z-50 origin-top-right transform scale-75 desktop-wide:scale-100" 
+        src="<?php echo get_template_directory_uri() . '/assets/svg/about-lines.svg' ?>" 
+        alt="Decorations"
+        style="max-width: 340px;"
+    />
+  <div class="pm-about pm-container pm-container--indented relative">
+    <img    
+        class="hidden desktop-wide:block absolute top-1/2 left-0 transform -translate-y-1/2 z-50" 
+        src="<?php echo get_template_directory_uri() . '/assets/svg/about-dots.svg' ?>" 
+        alt="Decorations"
+        style="max-width: 95px;"
+    />
+    <div class="flex flex-col tablet:flex-row">
+      <div class="flex-1/2">
+        <h2><?php echo get_field('about_title'); ?></h2>
+        <h4 class="pm-lead"><?php echo get_field('about_lead'); ?></h4>
+        <p><?php echo get_field('about_description'); ?></p>
+      </div>
+      <div class="flex items-end justify-end flex-1/2 desktop-wide:pr-64">
+      <?php $about_link = get_field('about_link'); ?>
+        <a
+            class="pm-button pm-button--transparent"
+            href="<?php echo $about_link['url'] ?>"
+            target="<?php echo $about_link['target'] ?>"
+            title="<?php echo $about_link['title'] ?>"
+        >
+            <?php echo $about_link['title'] ?>
+        </a>
+      </div>
+    </div>
+  </div>
 </section>
