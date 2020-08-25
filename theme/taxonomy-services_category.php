dcf3091b08e3526affaa1b16528bfd2feba056a3
Triloworld <patryk@padus.pl>
2020-08-25 14:38:57 +0200

    Taxonomy service category

--- theme/taxonomy-services_category.php
+++ theme/taxonomy-services_category.php
@@ -35,8 +35,10 @@ $sections_path = 'template-parts/section';
     $terms_all = [];
 
     foreach ($posts as $key => $post) {
-      if($terms_post = get_the_terms($post, 'services_tags') && !is_wp_error( $terms_post )){
-        $terms_all = array_unique(array_merge($terms_all, (array) $terms_post));
+      if($terms_post = get_the_terms($post, 'services_tags')){
+        if(!is_wp_error( $terms_post )){
+          $terms_all = array_unique(array_merge($terms_all, (array) $terms_post));
+        }
       }
     }
 
