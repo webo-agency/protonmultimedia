a91f1423a3d18ea3774e5b8d42a894123828c394
Tomek A <t.adamczyk@pureinteractive.pl>
2020-08-26 19:21:50 +0200

    header css adjustments, svg included

--- theme/template-parts/section-header.php
+++ theme/template-parts/section-header.php
@@ -1,10 +1,17 @@
-<header class="relative min-h-430px">
+<header class="relative min-h-200px tablet:min-h-430px mb-12">
     <?php
       if( $image = $args['background_id']) {
-        echo wp_get_attachment_image( $image, array('9999', '550'), "", array('class' => 'absolute left-0 top-0 tablet:relative w-full h-full object-cover') );
+        echo wp_get_attachment_image( $image, array('9999', '550'), "", array('class' => 'absolute left-0 top-0 w-full h-full object-cover') );
       }
     ?>
-  <div class="container absolute left-0 right-0 top-1/2 transform -translate-y-1/2">
+    <img 
+        class="hidden tablet-wide:block absolute -bottom-1/2 right-0 h-full z-50 w-auto pointer-events-none" 
+        src="<?php echo get_template_directory_uri() . '/assets/svg/header-lines.svg' ?>" 
+        width="540px"
+        height="400px"
+        alt="Decorations"
+    />
+  <div class="container absolute left-0 right-0 top-50px tablet:top-1/2 transform tablet:-translate-y-1/2">
     <h2 class="font-bold uppercase text-4xl tablet:text-8xl mb-4 tablet:mb-14 leading-tight">
       <?php 
         $lines = explode(' ', $args['title_string']);
