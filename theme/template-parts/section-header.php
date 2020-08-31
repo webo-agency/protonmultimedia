a8790dc456722e6ed87268372d7135dd6ee4e92e
Tomek A <t.adamczyk@pureinteractive.pl>
2020-08-31 20:56:12 +0200

    services header fix

--- theme/template-parts/section-header.php
+++ theme/template-parts/section-header.php
@@ -1,17 +1,17 @@
-<header class="relative overflow-hidden">
+<header class="relative">
     <?php
       if( $image = $args['background_id']) {
         echo wp_get_attachment_image( $image, array('9999', '550'), "", array('class' => 'absolute left-0 top-0 w-full h-full object-cover') );
       }
     ?>
     <img 
-        class="hidden tablet-wide:block absolute -bottom-1/2 right-0 h-full z-50 w-auto pointer-events-none" 
+        class="hidden tablet-wide:block absolute -bottom-40% right-0 h-full z-50 w-auto pointer-events-none" 
         src="<?php echo get_template_directory_uri() . '/assets/svg/header-lines.svg' ?>" 
         width="540px"
         height="400px"
         alt="Decorations"
     />
-  <div class="container absolute left-0 right-0 top-50px tablet:top-1/2 transform tablet:-translate-y-1/2 mt-40 mb-40">
+  <div class="container relative pt-40 pb-4">
     <h2 class="font-bold uppercase text-4xl tablet:text-8xl mb-4 tablet:mb-14 leading-tight">
       <?php 
         $lines = explode(' ', $args['title_string']);
@@ -30,16 +30,4 @@
       </div>
     </div>
   </div>
-  <?php
-    if( $image = $args['background_id']) {
-      echo wp_get_attachment_image( $image, array('9999', '550'), "", array('class' => 'absolute left-0 top-0 w-full h-full object-cover z-10') );
-    }
-  ?>
-  <img 
-      class="hidden tablet-wide:block absolute top-0 bottom-1/2 right-0 h-full w-auto transform -rotate-61 pointer-events-none z-20" 
-      src="<?php echo get_template_directory_uri() . '/assets/svg/header-lines.svg' ?>" 
-      width="540px"
-      height="400px"
-      alt="Decorations"
-  />
 </header>
