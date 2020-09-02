d39f88dbba0c64cbfe740fa623cc9af18a5a7279
Triloworld <patryk@padus.pl>
2020-09-02 15:28:57 +0200

    CTA

--- theme/template-parts/section-call-to-action.php
+++ theme/template-parts/section-call-to-action.php
@@ -6,33 +6,32 @@
             src="<?php echo get_template_directory_uri() . '/assets/svg/lines-right.svg' ?>" 
             alt="Decorations"
         />
-        <div class="container">
-            <div class="pm-cta__block">
-                <?php
-                    $cta_title = get_field('cta_title', 'option');
-                    $cta_button = get_field('cta_button', 'option');
-                
-                ?>
-                <div class="pm-cta__column mb-10 desktop:mb-0 uppercase">
-                    <h2 class="text-white"> 
-                        <?php 
-                            $lines = explode(PHP_EOL, $cta_title);
-                            foreach ( $lines as $line) {
-                                echo preg_replace("/\*(.+)\*/", '<span class="text-primary">$1</span>', $line);
-                            }
-                        ?>
-                    </h2>
-                </div>
-                <div class="pm-cta__column">
-                    <a
-                        class="pm-button pm-button--primary pm-button--large"
-                        href="<?php echo $cta_button['url'] ?>"
-                        target="<?php echo $cta_button['target'] ?>"
-                    >
-                        <?php echo $cta_button['title'] ?>
-                    </a>
-                </div>
+        <div class="container desktop:pl-72 flex flex-row flex-wrap desktop-wide:flex-no-wrap">
+            <?php
+                $cta_title = get_field('cta_title', 'option');
+                $cta_button = get_field('cta_button', 'option');
+            
+            ?>
+            <div class="flex-initial desktop-wide:w-7/12 mb-10 desktop:mb-0 uppercase">
+                <h2 class="mb-0 desktop:h2 desktop:flex desktop:flex-col text-white"> 
+                    <?php 
+                        $lines = explode(PHP_EOL, $cta_title);
+                        foreach ( $lines as $line) {
+                            echo preg_replace("/\*(.+)\*/", '<span class="text-primary">$1</span>', $line);
+                        }
+                    ?>
+                </h2>
             </div>
+            <div class="flex-initial desktop-wide:w-5/12">
+                <a
+                    class="pm-button pm-button--primary pm-button--large"
+                    href="<?php echo $cta_button['url'] ?>"
+                    target="<?php echo $cta_button['target'] ?>"
+                >
+                    <?php echo $cta_button['title'] ?>
+                </a>
+            </div>
+        
         </div>
     </div>
 </section>
