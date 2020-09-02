70b0f4220ff4bdb0b733e8a3a7f3f390cc88469d
Triloworld <patryk@padus.pl>
2020-09-03 00:06:55 +0200

    Post list

--- theme/template-parts/section-post-list-tiles.php
+++ theme/template-parts/section-post-list-tiles.php
@@ -17,58 +17,61 @@ function show_image( $image_id, $size = null, $attributes = null ) {
 }
 ?>
 <section class="container relative z-50">
-    <side-heading>
-      <h2><?php echo $args['section_name_string']; ?></h2>
-    </side-heading>
+  <side-heading>
+    <h2><?php echo $args['section_name_string']; ?></h2>
+  </side-heading>
+
+  <div class="guides left-minus-px right-auto bg-dark-blue"></div>
 
   <?php if(count($args['filters_array'])): ?>
-  <tag-filter>
-    <ul class="flex">
+    <tag-filter>
+      <ul class="flex">
+          <li class="mr-2" data-term="all">
+            <span class="pm-taxonomy-pill text-white">Wszystkie</span>
+          </li>
+        <?php foreach ($args['filters_array'] as $index => $term): ?>
+          <li class="mr-2" data-term="<?php echo $term->term_id; ?>">
+            <span class="pm-taxonomy-pill text-white"><?php echo $term->name; ?></span>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
     
-        <li class="mr-2" data-term="all">
-          <span class="pm-taxonomy-pill text-white">Wszystkie</span>
-        </li>
-      <?php foreach ($args['filters_array'] as $index => $term): ?>
-        <li class="mr-2" data-term="<?php echo $term->term_id; ?>">
-          <span class="pm-taxonomy-pill text-white"><?php echo $term->name; ?></span>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  <?php endif; ?>
-  
-  <?php if(count($args['list_post_array'])): ?>
-    <ul class="grid grid-cols-1 phone-wide:grid-cols-2 pt-12 tablet:pt-18 mb-10">
-      <?php foreach ($args['list_post_array'] as $index => $post): ?>
-        <li
-          data-terms="<?php $allterms = wp_get_post_terms( $post->ID, 'services_category'); 
-              foreach($allterms as $term) {
-                echo $term->term_id.',';
-              }
-          ?>"
-        >
-        <?php
-          $permalink = get_permalink( $post->ID );
-          $title = get_the_title( $post->ID );
-          $excerpt = get_the_excerpt( $post->ID);
-          $post_image = get_the_post_thumbnail_url($post->ID,'medium_large');
-          ?>
-          <a class="pm-category-item" href="<?php echo $permalink; ?>" title="<?php echo $title; ?>">
-            <?php
-              if( $image = get_field('service_list_icon')) {
-              ?>
-              <span class="pm-category-item__img svg-fill-primary">
-                <?php
-                  echo show_image( $image, array('9999', '550'), "", array('class' => 'w-full h-full object-cover') );
-                ?>
-              </span>
-              <?php
-              }
+    <?php if(count($args['list_post_array'])): ?>
+      <ul class="grid grid-cols-1 phone-wide:grid-cols-2 pt-12 tablet:pt-18 mb-10">
+        <?php foreach ($args['list_post_array'] as $index => $post): ?>
+          <li
+            data-terms="<?php $allterms = wp_get_post_terms( $post->ID, 'services_category'); 
+                foreach($allterms as $term) {
+                  echo $term->term_id.',';
+                }
+            ?>"
+          >
+          <?php
+            $permalink = get_permalink( $post->ID );
+            $title = get_the_title( $post->ID );
+            $excerpt = get_the_excerpt( $post->ID);
+            $post_image = get_the_post_thumbnail_url($post->ID,'medium_large');
             ?>
-            <span class="pm-category-item__title"><?php echo $title; ?></span>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </tag-filter>
+            <a class="pm-category-item h-full w-full" href="<?php echo $permalink; ?>" title="<?php echo $title; ?>">
+              <?php
+                if( $image = get_field('service_list_icon')) {
+                ?>
+                <span class="pm-category-item__img svg-fill-primary">
+                  <?php
+                    echo show_image( $image, array('9999', '550'), "", array('class' => 'w-full h-full object-cover') );
+                  ?>
+                </span>
+                <?php
+                }
+              ?>
+              <span class="pm-category-item__title"><?php echo $title; ?></span>
+            </a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </tag-filter>
   <?php endif; ?>
+
+  <div class="guides right-minus-px left-auto bg-dark-blue"></div>
 </section>
