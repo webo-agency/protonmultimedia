6de9cfa645a1f3159470a275e1201ad3961779c4
Triloworld <patryk@padus.pl>
2020-09-08 18:54:36 +0200

    Header animation

--- theme/template-parts/layout-header.php
+++ theme/template-parts/layout-header.php
@@ -7,13 +7,13 @@
 			<div class="site-branding z-30 w-32">
 				<?php the_custom_logo(); ?>
 			</div>
-			<button class="menu-toggle desktop:hidden z-40" data-menu aria-controls="primary-menu" aria-expanded="false">
+			<button class="menu-toggle block desktop:hidden z-40" data-menu aria-controls="primary-menu" aria-expanded="false">
 				<div class="w-8 h-1 bg-white rounded-lg mb-1"></div>
 				<div class="w-8 h-1 bg-white rounded-lg mb-1"></div>
 				<div class="w-8 h-1 bg-white rounded-lg"></div>
 			</button>
 			<main-navigation>
-				<nav id="site-navigation" class="main-navigation fixed left-0 top-0 w-full h-full hidden flex-col desktop:flex-row desktop:flex items-center flex-auto z-20 desktop:relative desktop:w-auto desktop:h-auto bg-dark-blue desktop:bg-transparent">
+				<nav id="site-navigation" class="main-navigation fixed left-0 top-0 w-full h-full transition-all duration-500 invisible opacity-0 desktop:opacity-100 desktop:visible flex flex-col desktop:flex-row desktop:flex items-center flex-auto z-20 desktop:relative desktop:w-auto desktop:h-auto bg-dark-blue desktop:bg-transparent">
 					<?php
 						wp_nav_menu(
 							array(
