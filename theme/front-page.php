ea06bb187b58717957c7083fadc15e9856f1d129
Triloworld <patryk@padus.pl>
2020-11-18 23:57:49 +0100

    Update deploy action

--- theme/front-page.php
+++ theme/front-page.php
@@ -23,7 +23,7 @@ $sections_path = 'template-parts/section';
 
 	<?php get_template_part( $sections_path, 'call-to-action' ); ?>
 
-	<!-- TEST deploy -->
+	<!-- TEST deploy 2 -->
 
 <?php
 get_footer();
