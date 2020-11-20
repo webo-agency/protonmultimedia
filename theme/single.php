cf8b68c04213c3f23e0aeb4684e35ac2f90d47f9
Triloworld <patryk@padus.pl>
2020-11-20 18:06:30 +0100

    PROTON-62 Posts template

--- theme/single.php
+++ theme/single.php
@@ -11,28 +11,27 @@
 
   $sections_path = 'template-parts/section';
 ?>
-	
-	<div id="primary" class="x-single content-area">
 		<main id="main" class="desktop-wide:px-smaller-container">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+				while ( have_posts() ) :
+					the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part( $sections_path, 'header', 
+						array( 
+							'title_string' => get_the_title('', false), 
+							'description_string' => get_the_content(),
+							'background_id' => get_post_thumbnail_id(),
+							'align_canter_boolean' => true
+						)
+					);
+			
+					get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+				endwhile;
+			?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
 
 	<?php get_template_part( $sections_path, 'call-to-action' ); ?>
 
