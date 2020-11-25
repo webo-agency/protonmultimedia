9440cbca60bcb08d105c392d783bad06627605bf
Triloworld <patryk@padus.pl>
2020-11-25 14:52:06 +0100

    Page 404

--- theme/404.php
+++ theme/404.php
@@ -32,7 +32,7 @@
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html( 'Oops! That page can&rsquo;t be found.', 'proton-theme' ); ?></h1>
-				</header><!-- .page-header -->
+				</header>
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'protonmultimedia-theme' ); ?></p>
