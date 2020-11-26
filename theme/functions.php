2096abde8accc140e35433c7ebcc59db3a9dcac4
Triloworld <patryk@padus.pl>
2020-11-26 17:28:47 +0100

    Gutenberg social link unwrap

--- theme/functions.php
+++ theme/functions.php
@@ -182,6 +182,10 @@ function wporg_block_wrapper( $block_content, $block ) {
 			$content .= '</div>';
 			return $content;
 	} 
+
+	if( $block['blockName'] === 'social-link') {
+		return $content;
+	}
 	
 	return $content;
 }
