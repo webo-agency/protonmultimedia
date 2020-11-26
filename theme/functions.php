ff7de6aecf93cf4da3a4b61e21429516f9c46170
Triloworld <patryk@padus.pl>
2020-11-26 16:12:30 +0100

    Limits gutenberg block

--- theme/functions.php
+++ theme/functions.php
@@ -185,3 +185,44 @@ function wporg_block_wrapper( $block_content, $block ) {
 }
 
 add_filter( 'render_block', 'wporg_block_wrapper', 10, 2 );
+
+
+function proton_allowed_block_types( $allowed_block_types, $post ) {
+	return array(
+	//	'core/archives',
+		'core/audio',
+		'core/button',
+	//	'core/categories',
+	//	'core/code',
+	//	'core/column',
+	//	'core/columns',
+		'core/coverImage',
+		'core/embed',
+		'core/file',
+		'core/freeform',
+		'core/gallery',
+		'core/heading',
+		'core/html',
+		'core/image',
+		'core/latestComments',
+		'core/latestPosts',
+		'core/list',
+		'core/more',
+		'core/nextpage',
+		'core/paragraph',
+		'core/preformatted',
+		'core/pullquote',
+		'core/quote',
+		'core/reusableBlock',
+		'core/separator',
+		'core/shortcode',
+		'core/spacer',
+		'core/subhead',
+		'core/table',
+		'core/textColumns',
+		'core/verse',
+		'core/video',
+	);
+}
+
+add_filter( 'allowed_block_types', 'proton_allowed_block_types', 10, 2 );
