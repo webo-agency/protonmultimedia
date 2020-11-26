473257fd40f0b42152110c91db8af9d648800c28
Triloworld <patryk@padus.pl>
2020-11-26 17:34:40 +0100

    Proper name for unwrap gutenberg block

--- theme/functions.php
+++ theme/functions.php
@@ -183,7 +183,7 @@ function wporg_block_wrapper( $block_content, $block ) {
 			return $content;
 	} 
 
-	if( $block['blockName'] === 'core/social-link') {
+	if( $block['blockName'] === 'core/social-links') {
 		return $content;
 	}
 	
