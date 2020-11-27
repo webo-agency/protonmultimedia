ab848662932682679014ccb3813c996ac4b27d58
Triloworld <patryk@padus.pl>
2020-11-27 19:27:12 +0100

    Template post header fix

--- theme/template-parts/section-post-header.php
+++ theme/template-parts/section-post-header.php
@@ -2,7 +2,7 @@
   <div class="container">
 
     <side-heading
-        class="z-20"
+        class="z-40 text-white"
         data-text="Blog"
     ></side-heading>
 
@@ -10,7 +10,7 @@
 
     <div class="image-overlay absolute top-0 bottom-0 left-0 w-full h-full -mb-1 z-20 pointer-events-none"></div>
     
-    <div class="max-w-900px mx-auto relative flex flex-col justify-center pt-40 pb-40 z-30">
+    <div class="max-w-900px mx-auto relative flex flex-col justify-center pt-40 pb-16 z-30">
       
       <span class="text-primary text-sm tablet:text-base leading-line-height-normal block mb-1 tablet:mb-2"><?php echo $args['post_date_string']; ?></span>
 
@@ -36,11 +36,13 @@
         </div>
       </div>
     </div>
+
     <?php
       if( $image = $args['background_id']) {
         echo wp_get_attachment_image( $image, array('9999', '550'), "", array('class' => 'absolute left-0 top-0 w-full h-full object-cover z-10') );
       }
     ?>
+    
     <img 
         class="hidden tablet-wide:block absolute top-0 bottom-1/2 right-0 h-full transform -rotate-61 pointer-events-none z-20 -mt-64" 
         src="<?php echo get_template_directory_uri() . '/assets/svg/header-lines.svg' ?>" 
