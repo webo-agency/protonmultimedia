4926b9b3c23161b6652dac28dfe6f007042989d8
Triloworld <patryk@padus.pl>
2020-11-27 20:36:35 +0100

    Post header spaces

--- theme/template-parts/section-post-header.php
+++ theme/template-parts/section-post-header.php
@@ -18,7 +18,7 @@
         <?php echo $args['title_string']; ?>
       </h2>
 
-      <div class="mb-6">
+      <div class="mb-16 pb-4">
         <?php
           foreach($args['post_terms_terms'] as $term) {
             ?>
