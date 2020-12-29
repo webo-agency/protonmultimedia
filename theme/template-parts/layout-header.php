39272e3f4293c5f7338120cfde88b5c4f08887d0
Triloworld <patryk@padus.pl>
2020-12-29 15:18:09 +0100

    Main menu - submenu

--- theme/template-parts/layout-header.php
+++ theme/template-parts/layout-header.php
@@ -22,7 +22,7 @@
 								'theme_location' => 'menu-2',
 								'menu_id'        => 'primary-menu',
 								'add_li_class' => 'transition-all duration-300 tablet:inline-block font-special mb-4 desktop:mb-0 uppercase font-semibold text-base desktop:mr-6',
-								'link_class'   => 'hover:text-primary transition-colors duration-300'
+								'link_class'   => 'hover:text-primary transition-colors duration-300 text-sm-2 font-semibold'
 							)
 						);
 					?>
