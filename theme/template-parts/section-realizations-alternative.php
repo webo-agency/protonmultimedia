081cf4777d5abfc470a4a227aebc3823a0b4758d
Triloworld <patryk@padus.pl>
2021-01-22 15:12:08 +0100

    PROTON-97 Gallery replace style

--- theme/template-parts/section-realizations-alternative.php
+++ theme/template-parts/section-realizations-alternative.php
@@ -43,7 +43,7 @@
                     <ul class="swiper-wrapper h-auto">
                         <?php while ( $loop->have_posts() ) : $loop->the_post(); $_post = get_post(); ?>
                             <li class="w-full h-auto swiper-slide flex flex-row flex-wrap tablet:block float-left">
-                                <div class="w-full desktop:w-1/2 overflow-hidden relative min-h-200px max-h-430px desktop:max-h-none object-bottom object-cover desktop:float-left desktop:pr-12">
+                                <div class="w-full desktop:w-1/2 overflow-hidden relative min-h-200px max-h-430px desktop:max-h-none object-bottom object-cover desktop:float-left desktop:pr-12 dekstop:pb-12">
                                     <div class="relative">
                                         <?php 
                                         $images = get_field('gallery_box');
