7c1e16af47c1ac96d1fc4f1da5d5aac8d01968d9
Triloworld <patryk@padus.pl>
2021-01-22 14:37:06 +0100

    PROTON-97 Gallery replace style

--- theme/template-parts/section-realizations-alternative.php
+++ theme/template-parts/section-realizations-alternative.php
@@ -40,9 +40,9 @@
         <div class="desktop:pb-32">
             <?php if( $loop->have_posts() ): ?>
                 <realization-alternative-slider class="z-30">
-                    <ul class="swiper-wrapper">
+                    <ul class="swiper-wrapper h-auto">
                         <?php while ( $loop->have_posts() ) : $loop->the_post(); $_post = get_post(); ?>
-                            <li class="w-full h-auto swiper-slide flex flex-row flex-wrap">
+                            <li class="w-full h-auto swiper-slide flex flex-row flex-wrap tablet:block float-left">
                                 <div class="w-full desktop:w-1/2 overflow-hidden relative min-h-200px max-h-430px desktop:max-h-none object-bottom object-cover">
                                     <?php 
                                     $images = get_field('gallery_box');
@@ -66,35 +66,7 @@
                                             ); 
                                         ?>
                                     <?php endif; ?>
-                                </div>
-                                <div class="w-full desktop:w-1/2 relative flex-auto flex flex-col flex-end p-4 desktop:pl-24 desktop:pr-32 desktop:pt-12 overflow-hidden z-30 justify-start">
-                                    <h3 class="relative mb-6 text-dark-font flex flex-col items-start desktop:mb-20">
-                                        <?php 
-                                            if(get_field('title_box')){
-                                                $lines = explode(PHP_EOL, get_field('title_box'));
-                                            } else {
-                                                $lines = explode(PHP_EOL, get_the_title( $_post ));
-                                            }
-                                            
-                                            foreach ( $lines as $line) {
-                                                echo preg_replace("/\*(.+)\*/", '<span class="line-decorated bg-primary text-white">$1</span>', $line);
-                                            }
-                                        ?>
-                                    </h3>
-                                        
-                                    <div class="text-base leading-tight text-dark-font">
-                                        <read-more more-text="Czytaj więcej" less-text="Czytaj mniej" link-class="text-primary">
-                                            <template v-slot:excerpt>
-                                                <?php echo the_excerpt(); ?>
-                                            </template>
 
-                                            <template v-slot:content>
-                                                <?php echo the_content(); ?>
-                                            </template>
-                                        </read-more>
-                                        
-                                    </div>
-                                
                                     <div class="flex items-center justify-between mb-0 mt-auto mx-auto desktop:ml-0 desktop:mr-auto">
                                         <div class="flex-initial px-4 py-8" data-realization-alternative-button-prev>
                                             <img 
@@ -116,8 +88,34 @@
                                             />  
                                         </div>
                                     </div>
-                                    
                                 </div>
+                                <div class="w-full desktop:w-1/2 relative flex-auto flex flex-col flex-end p-4 desktop:pl-24 desktop:pr-32 desktop:pt-12 overflow-hidden z-30 justify-start">
+                                    <h3 class="relative mb-6 text-dark-font flex flex-col items-start desktop:mb-20">
+                                        <?php 
+                                            if(get_field('title_box')){
+                                                $lines = explode(PHP_EOL, get_field('title_box'));
+                                            } else {
+                                                $lines = explode(PHP_EOL, get_the_title( $_post ));
+                                            }
+                                            
+                                            foreach ( $lines as $line) {
+                                                echo preg_replace("/\*(.+)\*/", '<span class="line-decorated bg-primary text-white">$1</span>', $line);
+                                            }
+                                        ?>
+                                    </h3>
+                                </div>
+
+                                <div class="text-base leading-tight text-dark-font">
+                                        <read-more more-text="Czytaj więcej" less-text="Czytaj mniej" link-class="text-primary">
+                                            <template v-slot:excerpt>
+                                                <?php echo the_excerpt(); ?>
+                                            </template>
+
+                                            <template v-slot:content>
+                                                <?php echo the_content(); ?>
+                                            </template>
+                                        </read-more>
+                                    </div>
                             </li>
                         <?php endwhile; ?>
                     </ul>
