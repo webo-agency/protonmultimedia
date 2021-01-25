9c8ce4c1ffd79534d579f6b606e2ec2e28fbce2a
Triloworld <patryk@padus.pl>
2021-01-25 17:41:40 +0100

    PROTON-97 Gallery replace style

--- theme/webpack.config.js
+++ theme/webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 const jsConfig = {
+    mode: 'development',
     target: "web",
     entry: {
         Main: ['./assets/js/main.js'],
