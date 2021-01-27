5900ad4cc4a3d2730b53e98680f9e4471101923b
Triloworld <patryk@padus.pl>
2021-01-27 12:02:18 +0100

    PROTON-97 Gallery replace style

--- theme/template-parts/section-realizations-alternative.php
+++ theme/template-parts/section-realizations-alternative.php
@@ -92,7 +92,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="w-full desktop:w-1/2 relative flex-auto flex flex-col flex-end p-4 desktop:pl-24 desktop:pr-32 desktop:pt-12 overflow-hidden z-30 justify-start desktop:float-left desktop:-ml-24">
+                                <div class="w-full desktop:w-1/2 relative flex-auto flex flex-col flex-end p-4 desktop:pl-24 desktop:pr-32 desktop:pt-12 overflow-hidden z-30 justify-start desktop:float-left desktop:-ml-24 desktop:mr-24">
                                     <h3 class="relative mb-6 text-dark-font flex flex-col items-start desktop:mb-20">
                                         <?php 
                                             if(get_field('title_box')){
